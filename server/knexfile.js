7f6a4fc8647286546db6e881df85b2085d60c906
Jason Loomis <jason@jrloomis.com>
2019-11-27 13:15:23 -0500

    require dotenv

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,44 +1,25 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
+  testing: {
+    client: 'pg',
+    connection: process.env.DATABASE_TEST,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: './data/migrations'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    seeds: {
+      directory: './data/seeds'
     }
   }
-
 };
