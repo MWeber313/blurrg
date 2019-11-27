45c39fd281a407ac1cf54c7ed1e7cf7803219384
Jason Loomis <jason@jrloomis.com>
2019-11-27 14:53:27 -0500

    graphql hooked up to basic server - CRUD working

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const playground = require('graphql-playground-middleware-express').default;
+
 const middleware = require('./middleware/global');
+const schema = require('./graphql/schema');
 
 const server = express();
 
 middleware(server);
 
+server.get('/playground', playground({ endpoint: '/graphql' }));
+
 server.get('/', (req, res) =>
   res.status(200).json({ everyone: 'good news there is' })
 );
+
+server.use('/graphql', graphqlHTTP({ schema, graphiql: false }));
 
 module.exports = server;
